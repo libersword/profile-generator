c181ca46ddc1ef23b9838e7b083e101bd22f610d
Lindsey Brown <libersword@gmail.com>
2019-11-09 10:59:05 -0700

    basic file set up, installed node modules

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
