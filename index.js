f693ddc2ff25c252833a3200b138401aaec25e4e
Lindsey Brown <libersword@gmail.com>
2019-11-09 13:34:32 -0700

    Created the index html file and styled the generated data

--- index.js
+++ index.js
@@ -45,26 +45,210 @@ function getGithub(){
     });
   }
   
-    function generateHTML(name, username, bio, location, profile, following, followers, repos, picture){
-      return  `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+  function generateHTML(name, username, bio, location, profile, following, followers, repos, picture) {
+    const colors = {
+      green: {
+        wrapperBackground: "#E6E1C3",
+        headerBackground: "#C1C72C",
+        headerColor: "black",
+        photoBorderColor: "#black"
+      },
+      blue: {
+        wrapperBackground: "#5F64D3",
+        headerBackground: "#26175A",
+        headerColor: "white",
+        photoBorderColor: "#73448C"
+      },
+      pink: {
+        wrapperBackground: "#879CDF",
+        headerBackground: "#FF8374",
+        headerColor: "white",
+        photoBorderColor: "#FEE24C"
+      },
+      red: {
+        wrapperBackground: "#DE9967",
+        headerBackground: "#870603",
+        headerColor: "white",
+        photoBorderColor: "white"
+      }
+    };
+    return `<!DOCTYPE html>
+  <html lang="en">
+     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
         <title>Document</title>
-      </head>
-      <body>
-        <h1>Name: ${name}</h1>
-        <h2>Username: ${username}</h2>
-        <h2>Location: ${location}</h2>
-        <h2>Bio: ${bio}</h2>
-        <h2>Profile: ${profile}</h2>
+        <style>
+            @page {
+              margin: 0;
+            }
+           *,
+           *::after,
+           *::before {
+           box-sizing: border-box;
+           }
+           html, body {
+           padding: 0;
+           margin: 0;
+           }
+           .wrapper {
+           background-color: ${colors.red.wrapperBackground};
+           padding-top: 100px;
+           }
+           body {
+           background-color: white;
+           -webkit-print-color-adjust: exact !important;
+           font-family: 'Cabin', sans-serif;
+           }
+           main {
+           background-color: #E9EDEE;
+           height: auto;
+           padding-top: 30px;
+           }
+           h1, h2, h3, h4, h5, h6 {
+           font-family: 'BioRhyme', serif;
+           margin: 0;
+           }
+           h1 {
+           font-size: 2em;
+           }
+           h2 {
+           font-size: 1.5em;
+           }
+           h3 {
+           font-size: 1em;
+           }
+           h4 {
+           font-size: 1em;
+           }
+           h5 {
+           font-size: 1em;
+           }
+           h6 {
+           font-size: 1em;
+           }
+           .photo-header {
+           position: relative;
+           margin: 0 auto;
+           margin-bottom: -50px;
+           display: flex;
+           justify-content: center;
+           flex-wrap: wrap;
+           background-color: ${colors.red.headerBackground};
+           color: ${colors.red.headerColor};
+           padding: 10px;
+           width: 95%;
+           border-radius: 6px;
+           }
+           .photo-header img {
+           width: 250px;
+           height: 250px;
+           border-radius: 50%;
+           object-fit: cover;
+           margin-top: -75px;
+           border: 6px solid ${colors.red.photoBorderColor};
+           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+           }
+           .photo-header h1, .photo-header h2 {
+           width: 100%;
+           text-align: center;
+           }
+           .photo-header h1 {
+           margin-top: 10px;
+           }
+           .links-nav {
+           width: 100%;
+           text-align: center;
+           padding: 20px 0;
+           font-size: 1.1em;
+           }
+           .nav-link {
+           display: inline-block;
+           margin: 5px 10px;
+           }
+           .workExp-date {
+           font-style: italic;
+           font-size: .7em;
+           text-align: right;
+           margin-top: 10px;
+           }
+           .container {
+           padding: 50px;
+           padding-left: 100px;
+           padding-right: 100px;
+           }
+  
+           .row {
+             display: flex;
+             flex-wrap: wrap;
+             justify-content: space-between;
+             margin-top: 20px;
+             margin-bottom: 20px;
+           }
+  
+           .card {
+            padding: 20px;
+            border-radius: 6px;
+            flex-basis: 45%;
+            background-color: #870603;
+            color: white;
+            margin: 0 auto;
+            margin-top: 100px;
+            margin-left: 30px;
+            margin-bottom: 30px;
+             background-color: ${colors.red.headerBackground};
+             color: ${colors.red.headerColor};
+           }
+           
+           .col {
+           flex: 1;
+           text-align: center;
+           }
+  
+           a, a:hover {
+           text-decoration: none;
+           color: inherit;
+           font-weight: bold;
+           }
+  
+           @media print { 
+            body { 
+              zoom: .75; 
+            } 
+           }
+        </style>
+        <body>
+        <div class="wrapper">
+              <div class="main">
+                <div class="photo-header">
+                <img src = ${picture}/>
+                <h1>${name}</h1>
+                <h2>${username}</h2>
+                <h2>${bio}</h2>
+                <div class="links-nav">
+                <div class="nav-link">
+                <a href='${profile}'>Github</a><br>
+                ${profile}
+                </div>
+                <div class="nav-link">
+                <a href="https://www.google.com/maps/place/${location}">Location</a><br>
+                ${location}
+                </div>
+        </div>
+        </div>
+        <div class="row">
+        <div class="card">
         <h2>Followers: ${followers}</h2>
         <h2>Following: ${following}</h2>
+        </div>
+        <div class="card">
         <h2>Repos: ${repos}</h2>
-        <img src = ${picture}/>
+        </div>
+        </div>
       </body>
       </html>
       `
